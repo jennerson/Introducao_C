9ac6b26ca8639edc20be32d5c2848dd6a0859b9b
jennerson <47647956+jennerson@users.noreply.github.com>
2019-04-02 21:42:00 -0300

    Update banco.c

--- Exercicios Práticos/banco.c
+++ Exercicios Práticos/banco.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <sdlib.h>
+#include <time.h>
 
+//Tipo conta bancaria
 typedef struct{
-	int NumConta;
-	char Cliente[100];
-	float Saldo;
-	int Senha;
-	char Chave;
+	int numConta;
+	char cliente[100];
+	float saldo;
+	char senha[8];
+	unsigned int chave;
 }contabancaria;
 
+//Lista de contas
 typedef struct{
 	contabancaria* conta;
 	Lista* prox;
@@ -19,7 +22,23 @@ Lista* CriaLista(){
 }
 
 contabancaria AddConta(){
+	int index;
+	
 	contabancaria conta = (contabancaria) malloc(sizeof(contabancaria));
+	srand(time(NULL));
+	
+	conta.Saldo = 0;
+		
+	conta.chave = rand() % 10000;
+	printf("Sua chave numérica é: %d\n")
+		  ("Guarde sua senha !!!", conta.chave);
+	
+	printf("Informe seu nome completo: ");
+	scanf("%s", conta.Cliente);
+	
+	printf("Crie sua senha: ");
+	scanf("%s", conta.senha);
+	
 	
 	return conta;
 }
